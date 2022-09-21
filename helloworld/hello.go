dc0d3fd6d1081c866a0e93aec14aa5e5b2041559
Chrystopher Medina <cmedina@wootbit.io>
2022-09-21 01:59:44 -0500

    add supoort for spanish and french, unsdertanding switch and constants

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,9 +2,36 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const spanish = "spanish"
+const french = "french"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+		if name == "" {
+			name = "Mundo"
+		}
+	case french:
+		prefix = frenchHelloPrefix
+		if name == "" {
+			name = "Monde"
+		}
+	}
+
+	if name == "" {
+		name = "World"
+	}
+
+	return prefix + name
 }
 func main() {
-	fmt.Println(Hello("Chrystopher"))
+	fmt.Println(Hello("Chrystopher", "french"))
 }
--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -3,9 +3,45 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chrystopher")
-	want := "Hello, Chrystopher"
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("Chrystopher", "")
+		want := "Hello, Chrystopher"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say in Spanish", func(t *testing.T) {
+		got := Hello("Chrystopher", "spanish")
+		want := "Hola, Chrystopher"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hola, Mundo' in spanish when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "spanish")
+		want := "Hola, Mundo"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say in French", func(t *testing.T) {
+		got := Hello("Chrystopher", "french")
+		want := "Bonjour, Chrystopher"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hola, Mundo' in french when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "french")
+		want := "Bonjour, Monde"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
